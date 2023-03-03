9e84cdf7a06b65f88d8a02e94e9ad5c2f660edd6
felixbeyonddigital <ruslanl1488@gmail.com>
2023-03-03 08:56:01 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Dashboard
+
+## _Test task_
+
+## Installation
+
+Dashboard requires [Node.js](https://nodejs.org/) v12+ to run.
+Install the dependencies and start the server.
+
+```sh
+cd test-task
+npm i
+```
+
+## Development
+
+Dashboard use Gulp for fast developing
+
+Please open your Terminal and run these commands
+
+For start the project:
+
+```sh
+gulp default
+```
+
+For build the project:
+
+```sh
+gulp build
+```
---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+// toggle sections
+let links = document.querySelectorAll(".links a");
+links.forEach((el) => {
+  el.addEventListener("click", checkActive, false);
+});
+
+const checkActive = function () {
+  document.querySelector(".links a.active").classList.remove("active");
+  this.classList.add("active");
+};
+
+const classHelper = function (selector, isToggle, clas) {
+  if (isToggle) {
+    document.querySelector(selector).classList.toggle(clas);
+  } else {
+    document.querySelector(selector).classList.remove(clas);
+  }
+};
+
+if (window.innerWidth <= 768) {
+  document.querySelector("#hamburger").addEventListener("click", function () {
+    classHelper(".aside", true, "active");
+    classHelper(".main", true, "active");
+    classHelper("body", true, "hidden");
+  });
+  document.querySelector(".main").addEventListener("click", function () {
+    classHelper(".aside", false, "active");
+    classHelper(".main", false, "active");
+    classHelper("body", false, "hidden");
+  });
+}
